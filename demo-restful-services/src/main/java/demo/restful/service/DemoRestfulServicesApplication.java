3d6d6fe844ffa33770c65d9f16e701f91b1c2b23
puneet098 <puneet098@gmail.com>
2020-04-01 23:15:47 +0530

    migration to intellj

--- demo-restful-services/src/main/java/demo/restful/service/DemoRestfulServicesApplication.java
+++ demo-restful-services/src/main/java/demo/restful/service/DemoRestfulServicesApplication.java
@@ -1,7 +1,14 @@
 package demo.restful.service;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class DemoRestfulServicesApplication {
@@ -9,5 +16,19 @@ public class DemoRestfulServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRestfulServicesApplication.class, args);
 	}
+	@Bean
+	public LocaleResolver  localeResolver() {
+		AcceptHeaderLocaleResolver locale=new AcceptHeaderLocaleResolver();
+		//SessionLocaleResolver locale = new SessionLocaleResolver();
+		locale.setDefaultLocale(Locale.US);
+		return locale;
+	}
+	
+	/*@Bean This can be removed as we have the property in application.properties
+	public ResourceBundleMessageSource resourceBundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}*/
 
 }
