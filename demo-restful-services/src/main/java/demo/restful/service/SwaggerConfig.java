249749ec4bcf906a9cbc94a9351652fb1ffb4a0f
puneet098 <puneet098@gmail.com>
2020-04-03 18:53:41 +0530

    swagger changes

---
+++ demo-restful-services/src/main/java/demo/restful/service/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package demo.restful.service;
+
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	
+	
+}
