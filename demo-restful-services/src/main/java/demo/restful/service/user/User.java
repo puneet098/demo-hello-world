19b24e3e13ab673f55ab2fa205760bcb2a3ee100
puneet098 <puneet098@gmail.com>
2020-04-05 21:29:52 +0530

    JPA integration

--- demo-restful-services/src/main/java/demo/restful/service/user/User.java
+++ demo-restful-services/src/main/java/demo/restful/service/user/User.java
@@ -4,7 +4,14 @@
 package demo.restful.service.user;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -14,14 +21,21 @@ import org.springframework.hateoas.RepresentationModel;
  * @author user
  *
  */
+@Entity
 public class User extends RepresentationModel<User>{
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	@Size(min=2,message="name should have atleast 2 Characters")
 	private String name;
+	
 	@Past
 	private Date birthDate;
-
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+	
 	protected User() {
 		
 	}
@@ -55,7 +69,13 @@ public class User extends RepresentationModel<User>{
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
